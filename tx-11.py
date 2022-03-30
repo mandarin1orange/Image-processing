9bd2c8b6ca7dd00b181d78c960c09e073191086d
mandarin1orange <100086932+mandarin1orange@users.noreply.github.com>
2022-03-30 23:09:37 +0800

    绘制验证码
    # ImageDraw
    # random

---
+++ tx-11.py
@@ -0,0 +1,36 @@
+from PIL import Image
+from PIL import ImageDraw
+import random
+
+pic = Image.new('RGB',(300,250),'white')
+draw_obj = ImageDraw.Draw(pic)
+
+# 获得随机颜色
+def get_color():
+    return (random.randint(200,255),random.randint(200,255),random.randint(200,255))
+
+# 生成随机字母
+def get_char():
+    return chr(random.randint(65,97))
+
+def main():
+
+    # 每个像素都是个花点
+    for x in range(300):
+        for y in range(250):
+            draw_obj.point((x,y),fill=get_color())
+
+    # 干扰线
+    for i in range(2):
+        draw_obj.line(((10,10),(80,80)),fill=(0,255,0),width=3)
+
+    # 生成随机字母
+    for i in range(4):
+        draw_obj.text((10+i*20,50),get_char(),fill=(255,0,0))
+
+    pic.show()
+
+    pass
+
+if __name__ == '__main__':
+    main()
